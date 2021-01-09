b0b0be7b8682ad2f32e68472ffd5fd75f924ae4e
ayushi2410 <ayushi241098@gmail.com>
2021-01-09 22:12:29 +0530

    new

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,17 @@
 import M from 'materialize-css';
 const Navbar=()=>{
 return(
-<div className="row container-navbar darken-1 blue-grey">
+<div className="row container-navbar darken-1 blue-grey navbar_sd">
 <nav>
   <div class="nav-wrapper row #607d8b blue-grey">
-    <div class="col-md-3 ml-md-auto">XYZ</div>
+    <div class="col-md-3 ml-md-auto">HR Manager</div>
     <div class="col-md-1 ml-md-auto">
-    <a href="#user"><img class="circle" src="../assets/newimg.jpg"/></a></div>
+    <a href="#">
+    <img class="circle" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLbYJBGzzSsLm1Hwvr0VSVkSnL4OmzabOz3w&usqp=CAU"/>
+   
+    </a>
+    
+    </div>
   </div>
 </nav>
 </div>
--- src/dummy.js
+++ src/dummy.js
@@ -42,3 +42,18 @@
            
         </div>*/
         /*start here */
+        /*
+side nav drawer code
+
+  <div className="container-fluid darken-1 blue-grey col drawer w-100">
+     <div className="w-100 white-text">Dashboard</div>
+     <div className="w-100 white-text">Add Employee</div>
+     <div className="w-100 white-text">General Management</div> 
+     <div className="w-100 white-text">Leaves</div>
+     <div className="w-100 white-text">Add user</div>
+     <div className="w-100 white-text">Generate reports</div>
+    </div>
+
+
+
+        */
